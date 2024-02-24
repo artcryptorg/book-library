69a14e2b1136fae0ab79e1a5b50a3338acd67b5f
alex1451 <aleksprodolls@gmail.com>
2024-02-25 00:43:19 +0100

    state-react

--- src/app.js
+++ src/app.js
@@ -5,6 +5,9 @@ class App {
     routes = [
         { path: "", view: MainView }
     ];
+    appState = {
+        favorites: []
+    } //глобальный и будет работать с фейворитс
 
     constructor() {
         window.addEventListener('hashchange', this.route.bind(this));
@@ -16,7 +19,7 @@ class App {
             this.currentView.destroy();
         }
         const view = this.routes.find(r => r.path == location.hash).view;
-        this.currentView = new view();
+        this.currentView = new view(this.appState);
         this.currentView.render(); // вызывает метод рендер вьюшки нужной нам
     }
 }
