fea4e18d6cf1e1ac0524d2b94f2fc5799a82d9f6
alex1451 <aleksprodolls@gmail.com>
2024-02-25 03:09:18 +0100

    big commit

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,4 +1,5 @@
 import { DivComponent } from "../../common/div-components";
+import './header/header.css';
 
 export class Header extends DivComponent {
     constructor(appState) {
@@ -11,6 +12,19 @@ export class Header extends DivComponent {
         this.el.innerHTML = `
 			<div>
 				<img src="/static/logo.svg" alt="Логотип" />
+			</div>
+			<div class="menu">
+				<a class="menu__item" href="#">
+					<img src="/static/search.svg" alt="Поиск иконка" />
+					Поиск книг
+				</a>
+				<a class="menu__item" href="#favorites">
+					<img src="/static/favorites.svg" alt="Избранное иконка" />
+					Избранное
+					<div class="menu__counter">
+						${this.appState.favorites.length}
+					</div>
+				</a>
 			</div>
 		`;
         return this.el;
