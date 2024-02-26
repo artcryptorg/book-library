cbe45e8718658b99c89eafc57935ef09fae45ef0
alex1451 <aleksprodolls@gmail.com>
2024-02-26 01:34:53 +0100

    search

--- src/components/search/search.js
+++ src/components/search/search.js
@@ -2,14 +2,19 @@ import { DivComponent } from '../../common/div-component';
 import './search.css';
 
 export class Search extends DivComponent {
-    constructor(state) {
-        super();
-        this.state = state;
-    }
+	constructor(state) {
+		super();
+		this.state = state;
+	}
 
-    render() {
-        this.el.classList.add('search');
-        this.el.innerHTML = `
+	search() {
+		const value = this.el.querySelector('input').value;
+		this.state.searchQuery = value;
+	}
+
+	render() {
+		this.el.classList.add('search');
+		this.el.innerHTML = `
 			<div class="search__wrapper">
 				<input
 					type="text"
@@ -21,6 +26,12 @@ export class Search extends DivComponent {
 			</div>
 			<button aria-label="Искать"><img src="./static/search-white.svg" alt="Иконка поиска" /></button>
 		`;
-        return this.el;
-    }
+		this.el.querySelector('button').addEventListener('click', this.search.bind(this));
+		this.el.querySelector('input').addEventListener('keydown', (event) => {
+			if (event.code === 'Enter') {
+				this.search();
+			}
+		});
+		return this.el;
+	}
 }
